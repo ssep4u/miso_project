03c0ab0918469a41ed23d41a13e91e358d71a051
SSep <ssep4u@gmail.com>
2017-04-27 15:44:13 +0900

    Add Word. models, admin, urls, views

--- miso/models.py
+++ miso/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Word(models.Model):
+    word = models.CharField(max_length=10, null=False)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.word+" at "+str(self.date)
+        # return self.word
--- miso/views.py
+++ miso/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Word
 
 def index(request):
     return render(request, 'index.html')
+
+def wordCloud(request):
+    w_list = Word.objects.all()
+    context = {'word_list':w_list}
+    return render(request, 'wordCloud.html', context)
