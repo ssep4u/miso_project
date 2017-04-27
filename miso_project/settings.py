b648821e2549450fb6287eb4a366ffe19050a5b8
SSep <ssep4u@gmail.com>
2017-04-27 10:15:12 +0900

    Modify urls.py

--- miso_project/settings.py
+++ miso_project/settings.py
@@ -21,10 +21,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "+!bu-1m=a$%(_8fs3!^(hsj_fny!&qk7*@0x_#&i)bzw&vw-u#"
+DEFAULT_SECRET_KEY = 'defaultSecretKey'
+SECRET_KEY = os.environ.get('SECRET_KEY',  DEFAULT_SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 # Application definition
 
--- miso_project/urls.py
+++ miso_project/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^miso/',include('miso.urls')),
+    url(r'^miso/', include('miso.urls')),
 ]
