cf156a434323957c45a1584385533844a38b27be
SSep <ssep4u@gmail.com>
2017-04-30 19:11:39 +0900

    Change admin address

--- miso_project/urls.py
+++ miso_project/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^_admin/', admin.site.urls),
     url(r'^miso/', include('miso.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
